6e771520fd45f0cc1efa1d1137653b6ccf0b3231
ryanwolfen7 <wolfen7lada@gmail.com>
2019-11-10 19:09:27 +0000

    added response object

--- controllers/Track.js
+++ controllers/Track.js
@@ -7,21 +7,16 @@ class TrackController {
         const { artist, title, index } = req.query
         const tracks = file.tracks 
 
-        if (artist) {
-            return res.json({
-                data: FindInArray(tracks, artist, 'artist')
-            }) 
+        if (artist) { 
+            return res.json({ data: FindInArray(tracks, artist, 'artist') }) 
         }
-        if (title) {
-            return res.json({
-                data: FindInArray(tracks, title, 'title')
-            })
+        if (title) { 
+            return res.json({ data: FindInArray(tracks, title, 'title') }) 
         }
-        if(index) {
-            return res.json({
-                data: FindInArray(tracks, index, 'id')
-            })
+        if (index) { 
+            return res.json({ data: FindInArray(tracks, index, 'id') })
         }
+        return res.json({ data: 'You have not provided a search query'})
     }
 }
 
